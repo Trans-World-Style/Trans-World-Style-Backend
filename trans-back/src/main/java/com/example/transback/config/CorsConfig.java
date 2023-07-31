45abafc606e14e32f1d5d786ba9e9ca2124c1d97
yllud <yllud75@gmail.com>
2023-07-31 18:54:03 +0900

    cors config

--- trans-back/src/main/java/com/example/transback/config/CorsConfig.java
+++ trans-back/src/main/java/com/example/transback/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.transback.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -9,11 +10,14 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
+    @Value("${cors.origin}")
+    private String allowedOrigin;
+
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("${cors.origin}");
+        config.addAllowedOrigin(allowedOrigin);
         config.addAllowedMethod("GET");
         config.addAllowedMethod("POST");
         config.addAllowedMethod("PUT");
