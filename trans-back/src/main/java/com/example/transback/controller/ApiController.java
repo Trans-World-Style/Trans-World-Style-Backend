1efa780ab6b53d3ab6f2fa452c171085c5836613
yllud <yllud75@gmail.com>
2023-08-10 14:59:10 +0900

    first commit

--- trans-back/src/main/java/com/example/transback/controller/ApiController.java
+++ trans-back/src/main/java/com/example/transback/controller/ApiController.java
@@ -1,13 +1,24 @@
 package com.example.transback.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ApiController {
+
+    @Value("${cors.origin}")
+    private String a;
 
     @GetMapping("/hello")
     public String hello(){
         return "hello";
+    }
+
+    @GetMapping("/hello2")
+    public String hello2(){
+        return a;
     }
 }
