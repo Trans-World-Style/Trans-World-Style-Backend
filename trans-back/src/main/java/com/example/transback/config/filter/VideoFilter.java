6a108092c9857f75846ea08094ebdb13d48a7732
yllud <yllud75@gmail.com>
2023-08-15 17:25:23 +0900

    jwt

--- trans-back/src/main/java/com/example/transback/config/filter/VideoFilter.java
+++ trans-back/src/main/java/com/example/transback/config/filter/VideoFilter.java
@@ -1,10 +1,12 @@
 package com.example.transback.config.filter;
 
+import com.example.transback.util.JwtUtil;
 import lombok.Data;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -18,15 +20,18 @@ public class VideoFilter extends AbstractGatewayFilterFactory<VideoFilter.Config
     @Override
     public GatewayFilter apply(Config config) {
         return ((exchange, chain) -> {
-            logger.info("VideoFilter baseMessage>>>>>>" + config.getBaseMessage());
-            if (config.isPreLogger()) {
-                logger.info("VideoFilter Start>>>>>>" + exchange.getRequest());
+            String jwt = exchange.getRequest().getHeaders().getFirst("Authorization");
+            jwt = jwt.replace("Bearer ", ""); // "Bearer " 접두사 제거
+            System.out.println(jwt);
+
+            if (jwt != null && JwtUtil.validateJWT(jwt)) {
+                // JWT가 유효한 경우 계속 필터 체인 진행
+                return chain.filter(exchange);
+            } else {
+                // 유효하지 않은 경우 401 Unauthorized 응답 반환
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
             }
-            return chain.filter(exchange).then(Mono.fromRunnable(()->{
-                if (config.isPostLogger()) {
-                    logger.info("VideoFilter End>>>>>>" + exchange.getResponse());
-                }
-            }));
         });
     }
 
