e05b7be26efedaa3c0d9688e2ea1c0375bc1bd24
yllud <yllud75@gmail.com>
2023-08-25 14:06:40 +0900

    video-test

--- trans-back/src/main/java/com/example/transback/config/WebConfig.java
+++ trans-back/src/main/java/com/example/transback/config/WebConfig.java
@@ -2,13 +2,8 @@ package com.example.transback.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.AntPathMatcher;
-import org.springframework.util.PathMatcher;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
