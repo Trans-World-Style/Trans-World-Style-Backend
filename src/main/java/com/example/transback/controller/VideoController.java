a48b3aa2042dba3842b54e6c3c9d2dc1272b0877
yllud <yllud75@gmail.com>
2023-10-05 16:52:51 +0900

    feat change uuid tag: test

--- src/main/java/com/example/transback/controller/VideoController.java
+++ src/main/java/com/example/transback/controller/VideoController.java
@@ -182,7 +182,9 @@ public class VideoController {
         String savedName0 = file.getOriginalFilename();
         String randomUUID = UUID.randomUUID().toString(); // 랜덤한 UUID 생성
 //        String savedName = randomUUID + savedName0;
-        String savedName = randomUUID+".mp4";
+//        String savedName = randomUUID+".mp4";
+        String savedName=savedName0;
+
         long videoSize = file.getSize();
 
 
