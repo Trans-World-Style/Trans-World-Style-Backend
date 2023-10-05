48ef4016b72faf562dabeca4866cbb0320f7383a
yllud <yllud75@gmail.com>
2023-10-05 17:02:04 +0900

    feat change uuid tag: test

--- src/main/java/com/example/transback/TransBackApplication.java
+++ src/main/java/com/example/transback/TransBackApplication.java
@@ -7,9 +7,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling // 추가
 @SpringBootApplication
 public class TransBackApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TransBackApplication.class, args);
 	}
 
 }
+
