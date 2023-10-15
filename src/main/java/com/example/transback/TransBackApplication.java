7f35d0ab253f5bca2eedc754b4896cd4616c2161
yllud <yllud75@gmail.com>
2023-10-15 14:49:45 +0900

    .

--- src/main/java/com/example/transback/TransBackApplication.java
+++ src/main/java/com/example/transback/TransBackApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling // 추가
+@EnableScheduling
 @SpringBootApplication
 public class TransBackApplication {
 	public static void main(String[] args) {
